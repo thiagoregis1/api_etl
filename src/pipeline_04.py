93d4bdbd263fdd583732f14003f99b36a428ef8e
Thiago Regis <thigorn@gmail.com>
2025-01-19 22:27:54 -0300

    update database_url

--- src/pipeline_04.py
+++ src/pipeline_04.py
@@ -29,10 +29,8 @@ POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = (
-    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}{POSTGRES_DB}"
-)
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 
